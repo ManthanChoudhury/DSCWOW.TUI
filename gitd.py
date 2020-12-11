54d812788ce88205f18e147e6cac1173f76b8492
root <root@localhost.localdomain>
2020-12-11 20:54:25 +0530

    alkej

--- gitd.py
+++ gitd.py
@@ -4,14 +4,13 @@ out = sb.getstatusoutput("sudo rpm -q git")
 if out[0] == 1:
         if 'not installed' in out[1]:
                 out = sb.getstatusoutput('sudo yum install git -y')
-                sb.call("echo 'git not found'", shell=True)
                 if 'complete!' in out[1]:
                         sb.call("echo 'Successfully installed git...'", shell=True)
 
 
 sb.call("rm -rf /home/ec2-user/DSCWOW.TUI/", shell=True)
 print("Cloning..")
-sb.call("git clone https://github.com/Prateek937/DSCWOW.TUI.git",shell=True)
+sb.call("git clone https://github.com/ManthanChoudhury/DSCWOW.TUI.git",shell=True)
 """if out[0] == 0:
         sb.call("echo 'repository cloned successfully...", shell=True)
 else:
