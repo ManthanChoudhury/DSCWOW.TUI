8ec6337d08cd16016c9895e36ca346cc159943d0
Prateek937 <prateeksanodiya937@gmail.com>
2020-12-11 22:44:48 +0530

    some

--- docker.py
+++ docker.py
@@ -2,75 +2,77 @@ import os
 
 
 def docker():
-    print("""
-                             Press 1 : To start docker
-                             Press 2 : To stop docker
-                             Press 3 : To check docker info
-                             Press 4 : To check docker help
-                             Press 5 : To download image 
-                             Press 6 : To check list of all images
-                             Press 7 : To run docker os  
-                             Press 8 : To run docker os in background
-                             Press 9 : To run docker os for single command
-                             Press 10 : To check current running os
-                             Press 11 : To check all running os 
-                             Press 12 : To exit 
-                             """)
-    ch = int(input("Enter what  u want to do : "))
-
-    if ch == 1:
-        os.system("systemctl start docker")
-        input()
-
-    elif ch == 2:
-        os.system("systemctl stop docker")
-        input()
-
-    elif ch == 3:
-        os.system("docker info")
-        input()
-
-    elif ch == 4:
-        os.system("docker --help")
-        input()
-    elif ch == 5:
+    chh = 0
+    while chh != 12:
         print("""
-                    image example 
-                    1. Centos 
-                    2. Fedora
-                    3. Ubuntu
-                    ...etc..
-                         """)
-        image = input("Enter the name of image ")
-        os.system("docker pull {}".format(image))
+                                 Press 1 : To start docker
+                                 Press 2 : To stop docker
+                                 Press 3 : To check docker info
+                                 Press 4 : To check docker help
+                                 Press 5 : To download image 
+                                 Press 6 : To check list of all images
+                                 Press 7 : To run docker os  
+                                 Press 8 : To run docker os in background
+                                 Press 9 : To run docker os for single command
+                                 Press 10 : To check current running os
+                                 Press 11 : To check all running os 
+                                 Press 12 : To go back
+                                 """)
+        ch = int(input("Enter what  u want to do : "))
 
-    elif ch == 6:
-        os.system("docker images")
-        input()
-    elif ch == 7:
-        name = input("Enter the name of os")
-        img = input("Enter the name of img")
-        os.system("docker run -it --name {} {}".format(name, img))
-    elif ch == 8:
-        name = input("Enter the name of os")
-        img = input("Enter the name of image")
-        os.system("docker run -dit --name {} {}".format(name, img))
-    elif ch == 9:
-        name = input("Enter the name of os")
-        img = input("Enter the name of image")
-        cmd = input("Enter the command u want to run ")
-        os.system("docker run -it --name {} {} {}".format(name, img, cmd))
-        input()
-    elif ch == 10:
-        os.system("docker ps")
-        input()
-    elif ch == 11:
-        os.system("docker ps -a")
-        input()
-    elif ch == 11:
-        exit()
-    else:
-        print("Not supported ")
+        if ch == 1:
+            os.system("systemctl start docker")
+            input()
+
+        elif ch == 2:
+            os.system("systemctl stop docker")
+            input()
+
+        elif ch == 3:
+            os.system("docker info")
+            input()
+
+        elif ch == 4:
+            os.system("docker --help")
+            input()
+        elif ch == 5:
+            print("""
+                        image example 
+                        1. Centos 
+                        2. Fedora
+                        3. Ubuntu
+                        ...etc..
+                             """)
+            image = input("Enter the name of image ")
+            os.system("docker pull {}".format(image))
+
+        elif ch == 6:
+            os.system("docker images")
+            input()
+        elif ch == 7:
+            name = input("Enter the name of os")
+            img = input("Enter the name of img")
+            os.system("docker run -it --name {} {}".format(name, img))
+        elif ch == 8:
+            name = input("Enter the name of os")
+            img = input("Enter the name of image")
+            os.system("docker run -dit --name {} {}".format(name, img))
+        elif ch == 9:
+            name = input("Enter the name of os")
+            img = input("Enter the name of image")
+            cmd = input("Enter the command u want to run ")
+            os.system("docker run -it --name {} {} {}".format(name, img, cmd))
+            input()
+        elif ch == 10:
+            os.system("docker ps")
+            input()
+        elif ch == 11:
+            os.system("docker ps -a")
+            input()
+        elif ch == 11:
+            exit()
+        else:
+            print("Not supported ")
 
 
 def docker_remote(ip):
--- linux.py
+++ linux.py
@@ -3,36 +3,38 @@ import os
 
 
 def linux():
-    print("""
-                 Press 1 : To run date
-                 Press 2 : To run calendar
-                 Press 3 : To check free ram
-                 Press 4 : To check ip address
-                 Press 5 : To exit from Prog
-                 """)
-    ch = int(input("Enter what  u want to do : "))
+    chh = 0
+    while chh != 5:
+        print("""
+                     Press 1 : To run date
+                     Press 2 : To run calendar
+                     Press 3 : To check free ram
+                     Press 4 : To check ip address
+                     Press 5 : To go back
+                     """)
+        ch = int(input("Enter what  u want to do : "))
 
-    if ch == 1:
-        os.system("date")
-        input()
-    elif ch == 2:
-        os.system("cal")
-        input()
+        if ch == 1:
+            os.system("date")
+            input()
+        elif ch == 2:
+            os.system("cal")
+            input()
 
-    elif ch == 3:
-        os.system("free -m")
-        input()
+        elif ch == 3:
+            os.system("free -m")
+            input()
 
-    elif ch == 4:
-        os.system("ifconfig enp0s3")
-        input()
+        elif ch == 4:
+            os.system("ifconfig enp0s3")
+            input()
 
-    elif ch == 5:
-        exit()
+        elif ch == 5:
+            exit()
 
-    else:
-        print("Not Supported")
-        input()
+        else:
+            print("Not Supported")
+            input()
 
 
 def linux_remote(ip):
